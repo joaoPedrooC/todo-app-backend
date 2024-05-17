3ff4e23543cb1da30d359ec6a21f6d47911a5552
João Pedro <jp871157@gmail.com>
2024-05-17 17:25:54 -0300

    feat: linked user router on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { json } from 'express'
 import helmet from 'helmet'
+import { userRouter } from './routes/user.routes'
 
 export const app = express()
 app.use(json())
 app.use(helmet())
+
+app.use('/users', userRouter)
