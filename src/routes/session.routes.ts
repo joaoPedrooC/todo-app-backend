461d83e3ff7b33041b24c885510d8d86d00a0983
João Pedro <jp871157@gmail.com>
2024-05-18 19:39:20 -0300

    feat: created session schema and put body validation on session route

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { SessionController } from "../controllers/session.controllers";
+import { ValidateBodyMiddleware } from "../middlewares/validateBody.middleware";
+import { sessionSchema } from "../schemas/session.schemas";
 
 export const sessionRouter = Router()
 
 const sessionController = new SessionController()
 
-sessionRouter.post('/', sessionController.login)
+sessionRouter.post('/', ValidateBodyMiddleware.execute(sessionSchema), sessionController.login)
