b85e25aa32b18397bf389dc97f73f047e5b982d7
João Pedro <jp871157@gmail.com>
2024-05-18 19:58:50 -0300

    fix: fixed user email verification logic

--- src/middlewares/verifyUserEmailExists.middleware.ts
+++ src/middlewares/verifyUserEmailExists.middleware.ts
@@ -4,7 +4,7 @@ import { AppError } from "../errors/AppError";
 
 export class VerifyUserEmailExists {
   static async execute(req: Request, res: Response, next: NextFunction) {
-    const searchingUser = await prisma.user.findFirst({ where: { email: req.params.email  } })
+    const searchingUser = await prisma.user.findFirst({ where: { email: req.body.email  } })
 
     if(searchingUser) throw new AppError('Email already exists', 409)
     
