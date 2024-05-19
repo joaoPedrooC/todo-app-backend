06a4af096034be04a3a8770ebc5a85c23011c884
João Pedro <jp871157@gmail.com>
2024-05-18 22:56:53 -0300

    feat: added todo router and necessary files

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import { HandleErrors } from './middlewares/handleErrors.middleware'
 
 import { userRouter } from './routes/user.routes'
 import { sessionRouter } from './routes/session.routes'
+import { todoRouter } from './routes/todo.routes'
 
 export const app = express()
 app.use(helmet())
@@ -13,5 +14,6 @@ app.use(json())
 
 app.use('/users', userRouter)
 app.use('/session', sessionRouter)
+app.use('/todos', todoRouter)
 
 app.use(HandleErrors.execute)
