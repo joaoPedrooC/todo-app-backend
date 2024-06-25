30f8f99fa15f91a2088db8f1feb0c4b05a592c08
João Pedro <jp871157@gmail.com>
2024-06-25 18:42:22 -0300

    fix: cors problem

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'express-async-errors'
 import express, { json } from 'express'
 import helmet from 'helmet'
+import cors, { CorsOptions } from 'cors'
 
 import { HandleErrors } from './middlewares/handleErrors.middleware'
 
@@ -8,9 +9,15 @@ import { userRouter } from './routes/user.routes'
 import { sessionRouter } from './routes/session.routes'
 import { todoRouter } from './routes/todo.routes'
 
+const corsOptions: CorsOptions = {
+  origin: 'http://localhost:5173'
+}
+
 export const app = express()
+
 app.use(helmet())
 app.use(json())
+app.use(cors())
 
 app.use('/users', userRouter)
 app.use('/session', sessionRouter)
